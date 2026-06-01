aa6f41f1c16c1bfed2b6487a0fe21aaa1aa34b72
W4samGG7 <00097524@uca.edu.sv>
2026-06-01 09:25:48 -0600

    feat: Agregar posible imagen de referencia usando AsyncImage

--- app/src/main/java/com/example/pdm_00097524/JSONPlaceholder/components/PostCard.kt
+++ app/src/main/java/com/example/pdm_00097524/JSONPlaceholder/components/PostCard.kt
@@ -4,15 +4,18 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
 import com.example.pdm_00097524.JSONPlaceholder.model.Post
 
 @Composable
@@ -26,6 +29,14 @@ fun PostCard(post: Post){
         Column(
             modifier = Modifier.padding(12.dp)
         ) {
+            /*
+            AsyncImage(
+                modifier = Modifier.size(90.dp),
+                alignment = Alignment.Center,
+                model = "https://images.unsplash.com/photo-1611915365928-565c527a0590?q=80&w=1025",
+                contentDescription = "Imagen de referencia"
+            )
+             */
             Text(text = "Numero post: ${post.id}")
             Text(text = "Usuario: ${post.userId}")
             Text(text = "titulo: ${post.title}",
